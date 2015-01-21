dc7de6ef66ce8d09b99a72a8061f796b3582cc83
vivian <vivian.xmt@gmail.com>
2015-01-21 13:20:05 +0800

    modify

--- Fight/MergeIntervals_56.java
+++ Fight/MergeIntervals_56.java
@@ -1,68 +1,76 @@
 package Fight;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
+/**
+ * sort and merge
+ * 
+ * @author Vivian
+ * 
+ */
 public class MergeIntervals_56 {
-    public List<Interval> merge(List<Interval> intervals) {
+	public List<Interval> merge(List<Interval> intervals) {
 
-        if (intervals == null || intervals.isEmpty() || intervals.size() == 1) {
-            return intervals;
-        }
+		if (intervals == null || intervals.isEmpty() || intervals.size() == 1) {
+			return intervals;
+		}
 
-        Collections.sort(intervals, new Comparator<Interval>() {
-            @Override
-            public int compare(Interval o1, Interval o2) {
-                return o1.start - o2.start;
-            }
-        });
+		Collections.sort(intervals, new Comparator<Interval>() {
+			@Override
+			public int compare(Interval o1, Interval o2) {
+				return o1.start - o2.start;
+			}
+		});
 
-//        for (int i = 0; i <= intervals.size() - 2; i++) {
-//            for (int j = i; j <= intervals.size() - 1; j++) {
-//                if (intervals.get(i).start > intervals.get(j).start) {
-//                    swap(intervals, i, j);
-//                }
-//            }
-//        }
+		List<Interval> res = new ArrayList<Interval>();
+		Interval current = null;
+		for (int i = 0; i <= intervals.size() - 1; i++) {
+			Interval tmp = intervals.get(i);
+			if (current == null) {
+				current = tmp;
+			} else {
+				if (tmp.start <= current.end) {
+					current.end = Math.max(current.end, tmp.end);
+				} else {
+					res.add(current);
+					current = tmp;
+				}
+			}
+		}
+		if (current != null) {
+			res.add(current);
+		}
+		return res;
+	}
 
-        List<Interval> res = new ArrayList<Interval>();
-        Interval current = null;
-        for (int i = 0; i <= intervals.size() - 1; i++) {
-            Interval tmp = intervals.get(i);
-            if (current == null) {
-                current = tmp;
-            } else {
-                if (tmp.start <= current.end) {
-                    current.end = Math.max(current.end, tmp.end);
-                } else {
-                    res.add(current);
-                    current = tmp;
-                }
-            }
-        }
-        if (current != null) {
-            res.add(current);
-        }
-        return res;
-    }
+	// for (int i = 0; i <= intervals.size() - 2; i++) {
+	// for (int j = i; j <= intervals.size() - 1; j++) {
+	// if (intervals.get(i).start > intervals.get(j).start) {
+	// swap(intervals, i, j);
+	// }
+	// }
+	// }
+	// private void swap(List<Interval> list, int i, int j) {
+	// Interval tmp = list.get(j);
+	// list.set(j, list.get(i));
+	// list.set(i, tmp);
+	// }
 
-//    private void swap(List<Interval> list, int i, int j) {
-//        Interval tmp = list.get(j);
-//        list.set(j, list.get(i));
-//        list.set(i, tmp);
-//    }
+	static class Interval {
+		int start;
+		int end;
 
-    private class Interval {
-        int start;
-        int end;
+		Interval() {
+			start = 0;
+			end = 0;
+		}
 
-        Interval() {
-            start = 0;
-            end = 0;
-        }
-
-        Interval(int s, int e) {
-            start = s;
-            end = e;
-        }
-    }
+		Interval(int s, int e) {
+			start = s;
+			end = e;
+		}
+	}
 }
