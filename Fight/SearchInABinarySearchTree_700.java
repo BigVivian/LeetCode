7eec880aafaa28ef30c6bc303af47b53b2873ce5
vivian <vivian.xmt@gmail.com>
2020-12-23 18:45:35 +0800

    Create SearchInABinarySearchTree_700.java

---
+++ Fight/SearchInABinarySearchTree_700.java
@@ -0,0 +1,14 @@
+package Fight;
+
+public class SearchInABinarySearchTree_700 {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root == null) return null;
+        if(root.val < val) {
+            return searchBST(root.right, val);
+        } else if(root.val > val) {
+            return searchBST(root.left, val);
+        } else {
+            return root;
+        }
+    }
+}
