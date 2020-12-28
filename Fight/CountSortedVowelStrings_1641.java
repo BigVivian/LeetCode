26e029dbaa6dbf4643b0ca66d8665b479234362a
vivian <vivian.xmt@gmail.com>
2020-12-28 09:35:23 +0800

    Update CountSortedVowelStrings_1641.java

--- Fight/CountSortedVowelStrings_1641.java
+++ Fight/CountSortedVowelStrings_1641.java
@@ -4,4 +4,14 @@ public class CountSortedVowelStrings_1641 {
     public int countVowelStrings(int n) {
         return (n+1) * (n+2) * (n+3) * (n+4)/24;
     }
+
+    public int countVowelStrings_2(int n) {
+        int[][] dp = new int[n+1][6];
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= 5; j++) {
+                dp[i][j] = dp[i][j-1] + (i == 1? 1 : dp[i-1][j]);
+            }
+        }
+        return dp[n][5];
+    }
 }
