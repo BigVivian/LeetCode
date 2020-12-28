bf2b077493c9018be0619a35cdb9a056baae94c3
vivian <vivian.xmt@gmail.com>
2020-12-28 09:35:28 +0800

    Update JumpGame_55.java

--- Fight/JumpGame_55.java
+++ Fight/JumpGame_55.java
@@ -24,4 +24,41 @@ public class JumpGame_55 {
 
         return true;
     }
+
+    // Greedy solution
+    public boolean canJump_2(int[] nums) {
+        int lastPos = nums.length - 1;
+
+        for(int i = nums.length - 1; i >= 0; i--) {
+            if(i + nums[i] >= lastPos) {
+                lastPos = i;
+            }
+        }
+
+        return lastPos == 0;
+    }
+
+    // bottom-up dynamic programming
+    enum Index{
+        GOOD, BAD, UNKNOWN
+    }
+
+    public boolean canJump_3(int[] nums) {
+        Index[] memo = new Index[nums.length];
+        for(int i = 0; i < nums.length; i++) {
+            memo[i] = Index.UNKNOWN;
+        }
+        memo[nums.length - 1] = Index.GOOD;
+
+        for(int i = nums.length - 2; i >= 0; i--) {
+            int furthest = Math.min(i + nums[i], nums.length - 1);
+            for(int next = i + 1; next <= furthest; next++) {
+                if(memo[next] == Index.GOOD) {
+                    memo[i] = Index.GOOD;
+                    break;
+                }
+            }
+        }
+        return memo[0] == Index.GOOD;
+    }
 }
