2e9c7133dfcb60554d1308bbf21d6b462cb7e142
vivian <vivian.xmt@gmail.com>
2020-12-29 08:34:06 +0800

    Create ReachANumber_754.java

---
+++ Fight/ReachANumber_754.java
@@ -0,0 +1,11 @@
+package Fight;
+
+public class ReachANumber_754 {
+    public int reachNumber(int target) {
+        target = Math.abs(target);
+        int k = 0;
+        while(target > 0)
+            target -= ++k;
+        return target % 2 == 0? k : k + 1 + k % 2;
+    }
+}
