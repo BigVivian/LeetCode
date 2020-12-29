08a91ad2d64430496cb36b8c5f72292ba6b815cc
vivian <vivian.xmt@gmail.com>
2020-12-29 08:45:46 +0800

    Update NumberOfStepsToReduceANumberToZero_1342.java

--- Fight/NumberOfStepsToReduceANumberToZero_1342.java
+++ Fight/NumberOfStepsToReduceANumberToZero_1342.java
@@ -1,12 +1,12 @@
 package Fight;
 
 public class NumberOfStepsToReduceANumberToZero_1342 {
-    public int numberOfSteps (int num) {
+    public int numberOfSteps(int num) {
         int ans = 0;
-        if(num == 0) return ans;
+        if (num == 0) return ans;
 
-        while(num != 0) {
-            if(num % 2 == 0) {
+        while (num != 0) {
+            if (num % 2 == 0) {
                 num /= 2;
             } else {
                 num -= 1;
@@ -14,5 +14,19 @@ public class NumberOfStepsToReduceANumberToZero_1342 {
             ans++;
         }
         return ans;
+    }
+
+    public int numberOfStep_2(int num) {
+        int res = 0;
+        if (num == 0) return res;
+
+        while (num != 0) {
+            // In order to count steps when right shifting one digit
+            // For odd: we will subtract and divide by 2, which takes 2 steps
+            // For even: we just divide by 2, which takes 1 step
+            res += ((num & 1) == 0) ? 1 : 2;
+            num >>= 1;
+        }
+        return res - 1;
     }
 }
