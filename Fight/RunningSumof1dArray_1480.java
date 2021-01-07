1616a1bce5320ee87db087d8e2c811eb8c80f992
vivian <vivian.xmt@gmail.com>
2021-01-07 15:57:43 +0800

    Create RunningSumof1dArray_1480.java

---
+++ Fight/RunningSumof1dArray_1480.java
@@ -0,0 +1,10 @@
+package Fight;
+
+public class RunningSumof1dArray_1480 {
+    public int[] runningSum(int[] nums) {
+        for(int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i-1];
+        }
+        return nums;
+    }
+}
