ff3624ff0b1347e428a9122ec33085acedd85c6e
vivian <vivian.xmt@gmail.com>
2021-01-15 19:31:24 +0800

    Update GroupthePeopleGiventheGroupSizeTheyBelongTo_1282.java

--- Fight/GroupthePeopleGiventheGroupSizeTheyBelongTo_1282.java
+++ Fight/GroupthePeopleGiventheGroupSizeTheyBelongTo_1282.java
@@ -4,28 +4,15 @@ import java.util.*;
 
 public class GroupthePeopleGiventheGroupSizeTheyBelongTo_1282 {
     public List<List<Integer>> groupThePeople(int[] groupSizes) {
+        List<List<Integer>> result = new LinkedList<>();
         Map<Integer, List<Integer>> map = new HashMap<>();
         for(int i = 0; i < groupSizes.length; i++) {
             int key = groupSizes[i];
-            if(map.containsKey(key)) {
-                List<Integer> list = map.get(key);
-                list.add(i);
-            } else {
-                List<Integer> list = new LinkedList<>();
-                list.add(i);
-                map.put(key, list);
-            }
-        }
-
-        List<List<Integer>> result = new LinkedList<List<Integer>>();
-        for(Integer key : map.keySet()) {
-            List<Integer> list = map.get(key);
-            for(int i = 0; i < list.size() / key; i++) {
-                List<Integer> group = new LinkedList<>();
-                for(int j = 0; j < key; j++) {
-                    group.add(list.get(i*key+j));
-                }
-                result.add(group);
+            List<Integer> list = map.computeIfAbsent(key, k -> new LinkedList<>());
+            list.add(i);
+            if(list.size() == key) {
+                result.add(list);
+                map.put(key, null);
             }
         }
         return result;
