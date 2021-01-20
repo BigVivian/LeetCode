4c42c9791b481a1932bdb31f4c564441a684153a
vivian <vivian.xmt@gmail.com>
2021-01-20 14:50:45 +0800

    Create SumofSquareNumbers_633.java

---
+++ Fight/SumofSquareNumbers_633.java
@@ -0,0 +1,17 @@
+package Fight;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SumofSquareNumbers_633 {
+    public boolean judgeSquareSum(int c) {
+        Set<Integer> set = new HashSet<>();
+        for(int i = 0; i <= Math.sqrt(c); i++) {
+            set.add(i * i);
+            if(set.contains(c - i * i)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
